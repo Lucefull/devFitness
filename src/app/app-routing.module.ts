3485b77879795970614db91233c846be0cc181d4
Lucefull <luansteffens@gmail.com>
2023-09-16 18:59:00 -0300

    feat: rota inicial login page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,17 +4,35 @@ import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 const routes: Routes = [
   {
     path: '',
-    loadChildren: () => import('./tabs/tabs.module').then(m => m.TabsPageModule)
+    loadChildren: () =>
+      import('./login-page/login-page.module').then(
+        (e) => e.LoginPagePageModule
+      ),
+  },
+  {
+    path: 'app',
+    loadChildren: () =>
+      import('./tabs/tabs.module').then((m) => m.TabsPageModule),
   },
   {
     path: 'cadastrar-treino',
-    loadChildren: () => import('./cadastrar-treino/cadastrar-treino.module').then( m => m.CadastrarTreinoPageModule)
-  }
+    loadChildren: () =>
+      import('./cadastrar-treino/cadastrar-treino.module').then(
+        (m) => m.CadastrarTreinoPageModule
+      ),
+  },
+  {
+    path: 'login-page',
+    loadChildren: () =>
+      import('./login-page/login-page.module').then(
+        (m) => m.LoginPagePageModule
+      ),
+  },
 ];
 @NgModule({
   imports: [
-    RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules })
+    RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules }),
   ],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
 export class AppRoutingModule {}
