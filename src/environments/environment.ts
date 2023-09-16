1a0b818b60196a13e3952cd09738036cf92e6584
Lucefull <luansteffens@gmail.com>
2023-09-16 18:52:12 -0300

    variaveis de ambiente

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,14 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  apiKey: 'AIzaSyDCN9Ou2GFqKYMS-awwVYaoXXrRfpQkLsU',
+  authDomain: 'devfitness-48eb8.firebaseapp.com',
+  databaseURL: 'https://devfitness-48eb8-default-rtdb.firebaseio.com',
+  projectId: 'devfitness-48eb8',
+  storageBucket: 'devfitness-48eb8.appspot.com',
+  messagingSenderId: '204317923569',
+  appId: '1:204317923569:web:d54c5319c498327c8b3150',
 };
 
 /*
