058f944bbbe9754928566228b1a599f8114444d0
Bruno Fuhr <bruno.fuhr@sap.com>
2023-09-17 20:40:18 -0300

    add treino

--- src/app/cadastrar-treino/cadastrar-treino.page.ts
+++ src/app/cadastrar-treino/cadastrar-treino.page.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-import { Exercicio } from 'src/model/estruturas';
+import { Exercicio, Treino } from 'src/model/estruturas';
+import { TreinoService } from '../services/treino.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-cadastrar-treino',
@@ -10,15 +12,14 @@ export class CadastrarTreinoPage implements OnInit {
   numeroSeries: string = "";
   nomeTreino: string ="";
   nomeExercicio: string ="";
-  numeroRepeticoes: string = "";  
+  numeroRepeticoes: string = "";
   exercicios: Exercicio[] = [];
 
-  constructor() { }
+  constructor(private treinoService: TreinoService, private router: Router) { }
 
-  ngOnInit() {    
+  ngOnInit() {
   }
-  doSomething() {
-    console.log('biriguiri')
+  adicionarExercicio() {
     let exercicio: Exercicio
     let series: number = +this.numeroSeries;
     let repeticoes: number = +this.numeroRepeticoes;
@@ -29,6 +30,13 @@ export class CadastrarTreinoPage implements OnInit {
     this.nomeExercicio =""
     this.numeroRepeticoes = ""
     this.numeroSeries = ""
+  }
+
+  salvar(){
+    this.treinoService.addTreino(this.nomeTreino, this.nomeTreino, 30, this.exercicios)
+    this.nomeTreino = "";
+    this.exercicios = [];
+    this.router.navigate(['']);
   }
 
 }
